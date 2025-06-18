e7de2c1ec0491eac17163390818adfdfa1b24a50
iWildGuns <hrdr1995@gmail.com>
2025-06-17 23:38:39 -0300

    subiendo nuevo proyecto en el que se integra una API para el Clima

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styles from "./App.module.css";
+import Form from "./Form/Form";
+
+function App() {
+  return (
+    <>
+      <h1 className={styles.tittle}>Buscador de clima</h1>
+      <div className={styles.container}>
+        <Form />
+        <p>2</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Form/Form.tsx
@@ -0,0 +1,53 @@
+import { countries } from "../data/countries";
+import styles from "./Form.module.css";
+import { useState, type ChangeEvent } from "react";
+import type { SearchType } from "../types";
+
+export default function Form() {
+  const [search, setSearch] = useState<SearchType>({
+    city: "",
+    country: "",
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <form className={styles.form}>
+      <div className={styles.field}>
+        <label htmlFor="city">Ciudad:</label>
+        <input
+          type="text"
+          id="city"
+          name="city"
+          placeholder="Ciudad"
+          value={search.city}
+          onChange={handleChange}
+        />
+      </div>
+      <div className={styles.field}>
+        <label htmlFor="country">País:</label>
+        <select
+          name="country"
+          id="country"
+          value={search.country}
+          onChange={handleChange}
+        >
+          <option value="">-- Seleccione un país --</option>
+          {countries.map((country) => (
+            <option key={country.code} value={country.code}>
+              {country.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <input className={styles.submit} type="submit" value="Consultar Clima" />
+    </form>
+  );
+}
